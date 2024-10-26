957733155ee9dd4939643e3ae206851e900a0c6a
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-10-27 04:55:17 +0900

    프로토 파일 로드

--- src/init/index.js
+++ src/init/index.js
@@ -1,8 +1,10 @@
 import { loadGameAssets } from './assets.js';
+import { loadProtos } from './loadProtos.js';
 
 const initServer = async () => {
   try {
-    await loadGameAssets;
+    await loadProtos();
+    await loadGameAssets();
   } catch (e) {
     console.error(e);
     process.exit(1); // 오류 발생 시 프로세스 종료
